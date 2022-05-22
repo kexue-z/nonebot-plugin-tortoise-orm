22d8328b9d690e61a6026eb65e3024310d778d70
kexue <x@kexue.io>
2022-05-22 16:01:33 +0800

    ✨ 自动创建空数据库

--- nonebot_plugin_tortoise_orm/__init__.py
+++ nonebot_plugin_tortoise_orm/__init__.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from nonebot import get_driver
 from tortoise import Tortoise
 from nonebot.log import logger
@@ -7,16 +9,21 @@ from .config import Config
 db_config = Config.parse_obj(get_driver().config.dict())
 driver = get_driver()
 
-moduls = []
+db_url = db_config.db_url
+
+if not db_url or "":
+    logger.opt(colors=True).warning("<y>没有数据库地址, 将会初始化数据库到 db.sqlite3</y>")
+    db_url = "sqlite://db.sqlite3"
+
+moduls: List[str] = []
 
 
 @driver.on_startup
 async def connect():
-    await Tortoise.init(db_url=db_config.db_url, modules={"models": moduls})
+
+    await Tortoise.init(db_url=db_url, modules={"models": moduls})
+    await Tortoise.generate_schemas()
     logger.opt(colors=True).success("<y>数据库: 连接成功</y>")
-    if db_config.db_generate_schemas:
-        logger.opt(colors=True).success("<y>数据库: 初始化</y>")
-        await Tortoise.generate_schemas()
 
 
 @driver.on_shutdown
--- nonebot_plugin_tortoise_orm/config.py
+++ nonebot_plugin_tortoise_orm/config.py
@@ -1,6 +1,7 @@
+from typing import Optional
+
 from pydantic import Extra, BaseModel
 
 
 class Config(BaseModel, extra=Extra.ignore):
-    db_url: str
-    db_generate_schemas: bool = False
+    db_url: Optional[str]
