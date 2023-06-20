5ad069832708e040374967ddff078d53fdaa2ac8
kexue <xana278@foxmail.com>
2023-06-20 11:28:25 +0800

    ✨

--- nonebot_plugin_tortoise_orm/__init__.py
+++ nonebot_plugin_tortoise_orm/__init__.py
@@ -1,27 +1,20 @@
 from typing import List
 
-from nonebot import get_driver
+from nonebot import require, get_driver
 from tortoise import Tortoise
 from nonebot.log import logger
 
-from .config import Config
+from .config import DB_URL
 
-db_config = Config.parse_obj(get_driver().config.dict())
+require("nonebot_plugin_localstore")
 driver = get_driver()
-
-db_url = db_config.db_url
-
-if not db_url or "":
-    logger.opt(colors=True).warning("<y>没有数据库地址, 将会初始化数据库到 db.sqlite3</y>")
-    db_url = "sqlite://db.sqlite3"
 
 moduls: List[str] = []
 
 
 @driver.on_startup
 async def connect():
-
-    await Tortoise.init(db_url=db_url, modules={"models": moduls})
+    await Tortoise.init(db_url=DB_URL, modules={"models": moduls})
     await Tortoise.generate_schemas()
     logger.opt(colors=True).success("<y>数据库: 连接成功</y>")
 
--- nonebot_plugin_tortoise_orm/config.py
+++ nonebot_plugin_tortoise_orm/config.py
@@ -1,7 +1,21 @@
-from typing import Optional
-
-from pydantic import Extra, BaseModel
+from typing import Dict
+from pathlib import Path
+from nonebot.log import logger
+from nonebot import get_driver
+from pydantic import Extra, BaseModel, root_validator
+from nonebot_plugin_localstore import get_data_dir
 
 
 class Config(BaseModel, extra=Extra.ignore):
-    db_url: Optional[str]
+    db_url: str
+    datastore_data_dir: Path
+
+    @root_validator(pre=True, allow_reuse=True)
+    def set_default(cls, value: Dict):
+        if not value.get("db_url"):
+            value["db_url"] = f'sqlite:///{get_data_dir("") / "db.sqlite3"}'
+            logger.warning(f"没有设置数据库地址, 使用 {value['db_url']}")
+
+
+plugin_config = Config.parse_obj(get_driver().config)
+DB_URL = plugin_config.db_url
