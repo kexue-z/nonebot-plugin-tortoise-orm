c4c42bd9b79e5a0627b206cda97045ab55bf2450
kexue <xana278@foxmail.com>
2023-08-22 11:42:23 +0800

    ✨  添加多数据库兼容

--- nonebot_plugin_tortoise_orm/__init__.py
+++ nonebot_plugin_tortoise_orm/__init__.py
@@ -1,7 +1,8 @@
 from nonebot import require, get_driver
 
 require("nonebot_plugin_localstore")
-from typing import List
+from json import dumps
+from typing import List, Optional
 
 from tortoise import Tortoise
 from nonebot.log import logger
@@ -20,16 +21,35 @@ __plugin_meta__ = PluginMetadata(
 
 driver = get_driver()
 
-moduls: List[str] = []
+
+DATABASE = {
+    "connections": {"default": DB_URL},
+    "apps": {
+        "default": {
+            "models": [],
+            "default_connection": "default",
+        }
+    },
+    "use_tz": False,
+    "timezone": "Asia/Shanghai",
+}
+
+models: List[str] = []
 
 
 @driver.on_startup
 async def connect():
-    await Tortoise.init(db_url=DB_URL, modules={"models": moduls})
+    # await Tortoise.init(db_url=DB_URL, modules={"models": moduls})
+    DATABASE["apps"]["default"]["models"] = models
+
+    logger.debug("参数预览\n" + dumps(DATABASE, ensure_ascii=False, indent=4))
+
+    await Tortoise.init(DATABASE)
     await Tortoise.generate_schemas()
-    logger.opt(colors=True).success(
-        f"<y>数据库: 连接成功</y> URL: <r>{DB_URL.split('@',maxsplit=1)[-1]}</r>"
-    )
+
+    for db in DATABASE["connections"].keys():
+        db_url = DATABASE["connections"][db].split("@", maxsplit=1)[-1]
+        logger.opt(colors=True).success(f"<y>数据库: {db} 连接成功</y> URL: <r>{db_url}</r>")
 
 
 @driver.on_shutdown
@@ -38,6 +58,33 @@ async def disconnect():
     logger.opt(colors=True).success("<y>数据库: 断开链接</y>")
 
 
-def add_model(model: str):
-    logger.opt(colors=True).success(f"<y>数据库: 添加模型</y>: <r>{model}</r>")
-    moduls.append(model)
+def add_model(model: str, db_name: Optional[str] = None, db_url: Optional[str] = None):
+    """
+    :说明: `add_model`
+    > 添加数据模型
+
+    :参数:
+      * `model: str`: 模型位置/名称
+            - 如果发布插件可以为 `__name__` 或 插件名称.模型所在文件 例如 `abc.models`
+            - 如果是作为 Bot 插件形式 需按照相对路径填写 例如 `plugins.test.models`
+
+    :可选参数: 该可选参数必须同时使用
+
+    仅在需要自定义某插件独立数据库时需要，否则将会使用默认 `db_url` 存放在默认位置
+
+      * `db_name: Optional[str] = None`: 数据库名称
+      * `db_url: Optional[str] = None`: 数据库URL [参考](https://tortoise.github.io/databases.html#db-url)
+    """
+    if bool(db_name) != bool(db_url):
+        raise TypeError("db_name 和 db_url 必须同时为 None 或 str")
+
+    if db_name and db_url:
+        DATABASE["connections"][db_name] = db_url
+        DATABASE["apps"][db_name] = {"models": []}
+        DATABASE["apps"][db_name]["default_connection"] = db_name
+        DATABASE["apps"][db_name]["models"].append(model)
+        logger.opt(colors=True).success(f"<y>数据库: 添加模型 {db_name}</y>: <r>{model}</r>")
+
+    else:
+        models.append(model)
+        logger.opt(colors=True).success(f"<y>数据库: 添加模型</y>: <r>{model}</r>")
